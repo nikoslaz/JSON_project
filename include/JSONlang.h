10ab8b802a46ebb9887b6bd6f64329ce457ed412
Nikos Lazaridis <nikoslaz@ics.forth.gr>
2024-12-16 12:28:57 +0200

    pr

--- include/JSONlang.h
+++ include/JSONlang.h
@@ -1,8 +1,38 @@
 #ifndef JSONLANG.H
 #define JSONLANG.H
 
+#define JSON(name) JsonValue name
+#define STRING(value) std::string(value)
+#define NUMBER(value) 
+#define TRUE true
+#define FALSE false
+#define NULLVALUE nullptr
+#define OBJECT JsonObject
+#define ARRAY JsonArray
+#define KEY(key) key
 
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <variant>
 
+using namespace std;
+
+/* Map of key-value pairs */
+struct JsonObject;
+
+/* Vector of JSON values */
+struct JsonArray;
+
+/* Define types for JSON values */
+typedef variant<string, double, bool, nullptr_t, struct JsonObject, struct JsonArray> JsonValue;
+
+template<typename... Args>
+void PRINT(Args... args) {
+    ((std::cout << args << " "), ...);
+    std::cout << std::endl;
+}
 
 #endif //JSONLANG.H
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
-#include <iostream>
+#include "../include/JSONlang.h"
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    // Define JSON variables
+    JSON(book) = OBJECT{
+        {KEY("title"), STRING("Gone Girl")},
+        {KEY("published"), NUMBER(2012)},
+        {KEY("type"), STRING("Thriller")},
+        {KEY("author"), OBJECT{
+            {KEY("firstname"), STRING("Gillian")},
+            {KEY("sirname"), STRING("Flynn")},
+            {KEY("age"), NUMBER(45)}
+        }}
+    };
+ 
+    
     return 0;
 }
