b173367503dc3bc8c9f7848593b0c5177ff0908c
ZarOoO13 <csd4875@csd.uoc.gr>
2024-12-16 11:50:15 +0200

    namespace

---
+++ build/CMakeFiles/JSON_Project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JSON_Project.dir/src/main.cpp.o"
+  "CMakeFiles/JSON_Project.dir/src/main.cpp.o.d"
+  "bin/JSON_Project"
+  "bin/JSON_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JSON_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
