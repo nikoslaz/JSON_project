8c5d449d8807fc905f8818fede8b79ab4293cda3
Nikos Lazaridis <nikoslaz@ics.forth.gr>
2024-12-29 13:01:15 +0200

    Beginning

--- include/JSONlang.h
+++ include/JSONlang.h
@@ -1,19 +1,12 @@
-#ifndef JSONLANG_H
-#define JSONLANG_H
-
 #include <iostream>
-#include <vector>
 #include <unordered_map>
+#include <vector>
 #include <string>
-#include <variant>
-#include <stdexcept>
 
 using namespace std;
 
-using JsonObject = unordered_map<string, class JsonValue>;
-using JsonArray = vector<class JsonValue>;
-
-enum class JsonType : short {
+enum class JSONType : short
+{
     NUMBER,
     STRING,
     NULLT,
@@ -23,125 +16,41 @@ enum class JsonType : short {
 };
 
 class JsonValue {
-private:
-    JsonType type;
-    JsonObject object;
-    JsonArray array;
+    JSONType d_type;
+    unordered_map<string, JsonValue> d_object;
+    vector <JsonValue> d_array;
 
-    // Replace union with variant
-    using JsonValueType = variant<monostate, string, double, bool>;
-    JsonValueType j_value;
+    // union to hold the value if there is a value.
+    union value
+    {
+        string d_string;
+        double d_number;
+        bool d_bool;
+        value() {}
+        ~value() {}
 
-    void limitToObject() {
-        if (!isObject()) {
-            throw runtime_error("this operation is only available to object node");
+        // conversion operators for the union
+        operator std::string()
+        {
+            return d_string;
         }
-    }
 
-    void limitToArray() {
-        if (!isArray()) {
-            throw runtime_error("this operation is only available to array node");
+        operator double()
+        {
+            return d_number;
         }
-    }
 
-public:
-    // Constructors
-    JsonValue(JsonType type) : type(type) {}
-
-    JsonValue() : type(JsonType::NULLT), j_value(monostate{}) {}
-
-    JsonValue(nullptr_t) : JsonValue() {}
-
-    JsonValue(const vector<JsonValue>& nodes) : type(JsonType::ARRAY), array(nodes) {}
-
-    JsonValue(double value) : type(JsonType::NUMBER), j_value(value) {}
-
-    JsonValue(const string& value) : type(JsonType::STRING), j_value(value) {}
-
-    JsonValue(bool value) : type(JsonType::BOOL), j_value(value) {}
-
-    // Copy constructor
-    JsonValue(const JsonValue& node) : type(node.type), object(node.object), array(node.array), j_value(node.j_value) {}
-
-    // Copy assignment operator
-    JsonValue& operator=(const JsonValue& node) {
-        if (this != &node) {
-            type = node.type;
-            object = node.object;
-            array = node.array;
-            j_value = node.j_value;
+        operator int()
+        {
+            return d_number;
         }
-        return *this;
-    }
 
-    JsonValue& operator=(const JsonObject& node) {
-            object = node;
-        return *this;
-    }
-
-    // Member functions
-    bool isValue() const {
-        return type == JsonType::BOOL || type == JsonType::NUMBER || type == JsonType::STRING || type == JsonType::NULLT;
-    }
-
-    bool isObject() const {
-        return type == JsonType::OBJECT;
-    }
-
-    bool isArray() const {
-        return type == JsonType::ARRAY;
-    }
-
-    bool isNULL() const {
-        return type == JsonType::NULLT;
-    }
-
-    void appendArray(const JsonValue& node) {
-        if (!isArray()) {
-            throw runtime_error("appendArray is only available for array nodes");
+        operator bool()
+        {
+            return d_bool;
         }
-        array.push_back(node);
-    }
 
-    template<typename T>
-    T get() const {
-        if (!isValue()) {
-            throw runtime_error("unable to get value for this type");
-        }
-        return get<T>(j_value);
-    }
+    } d_value;
 
-    // Index operator overloads
-    JsonValue& operator[](int index) {
-        limitToArray();
-        return array[index];
-    }
-
-    JsonValue& operator[](const string& key) {
-        limitToObject();
-        return object[key];
-    }
-
-    JsonValue& operator[](const char* key) {
-        return (*this)[string(key)];
-    }
-
-    // Conversion operator overloads
-    operator string() const {
-        return get<string>();
-    }
-
-    operator int() const {
-        return static_cast<int>(get<double>());
-    }
-
-    operator double() const {
-        return get<double>();
-    }
-
-    operator bool() const {
-        return get<bool>();
-    }
+    
 };
-
-#endif // JSONLANG_H
