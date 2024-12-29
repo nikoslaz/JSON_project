0ac5cd81063380dcb5b787d0979c9daad6700354
Nikos Lazaridis <nikoslaz@ics.forth.gr>
2024-12-29 13:14:48 +0200

    Update JSONlang.h

--- include/JSONlang.h
+++ include/JSONlang.h
@@ -52,5 +52,84 @@ class JsonValue {
 
     } d_value;
 
+    void limitToArray()
+    {
+        if (!isArray())
+        {
+            throw runtime_error("this operation is only available to array node");
+        }
+    }
+
+    void limitToObject()
+    {
+        if (!isObject())
+        {
+            throw runtime_error("this operation is only available to object node");
+        }
+    }
+
+public: 
+
+    JsonValue(JSONType type) : d_type(type) {}
+
+    // default constructor creates null value node
+    JsonValue() : d_type(JSONType::NULLT) {}
+
+    // constructors for implicit conversion from nullptr
+    JsonValue(nullptr_t value) : JsonValue() {}
+
+    JsonValue(double value) : d_type(JSONType::NUMBER)
+    {
+        d_value.d_number = value;
+    }
+
+    JsonValue(const vector<JsonValue> &nodes) : d_type(JSONType::ARRAY), d_array(nodes) {}
+
+    JsonValue(int value) : d_type(JSONType::NUMBER)
+    {
+        d_value.d_number = value;
+    }
+
+    JsonValue(const string &value) : d_type(JSONType::STRING)
+    {
+        d_value.d_string = value;
+    }
+
+    JsonValue(const char *value) : d_type(JSONType::STRING)
+    {
+        d_value.d_string = value;
+    }
+
+    JsonValue(bool value) : d_type(JSONType::BOOL)
+    {
+        d_value.d_bool = value;
+    }
+
+    // copy constructor
+    JsonValue(const JsonValue &node)
+    {
+        d_type = node.d_type;
+        d_object = node.d_object;
+        d_array = node.d_array;
+
+        d_value.d_bool = node.d_value.d_bool;
+        d_value.d_string = node.d_value.d_string;
+        d_value.d_number = node.d_value.d_number;
+    }
+
+    // copy assignment operator 
+    JsonValue &operator=(const JsonValue &node)
+    {
+        d_type = node.d_type;
+        d_object = node.d_object;
+        d_array = node.d_array;
+
+        d_value.d_bool = node.d_value.d_bool;
+        d_value.d_string = node.d_value.d_string;
+        d_value.d_number = node.d_value.d_number;
+
+        return *this;
+    }
+
 
 };
